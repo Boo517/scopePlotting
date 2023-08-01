46f065c57bcbd954c8577c9aef18c30e01feb064
unknown <noahdilworth517@gmail.com>
2023-08-01 15:56:35 -0700

    changed column dict to reflect imported data array rather than raw

--- scopePlotting.py
+++ scopePlotting.py
@@ -18,21 +18,27 @@ def getfile():
     file_path = tkFileDialog.askopenfilename(parent=root,title='Pick a file')    
     return file_path 
 
-#this dictionary gives which column a certain data channel lies on
+#this dictionary gives which column a certain data channel lies on in the
+#data array 
 #woulda used an enum if they existed in python
 columns = {
-    'sample' : 0,   #a string, either "DSO1" or "DSO2" and its sample #
-    'trigger' : 1,  #[V]trigger signal
-    'rog1' : 2,     #[V]rogowski coil 1
-    'rog2' : 3,     #[V]rogowski coil 2
-    'diode' : 4,    #[V]diode for laser timing
-    'time' : 9      #[ps]timestamp of sample
+    'trigger' : 0,  #[V]trigger signal
+    'rog1' : 1,     #[V]rogowski coil 1
+    'rog2' : 2,     #[V]rogowski coil 2
+    'diode' : 3,    #[V]diode for laser timing
+    'DSO21': 4,
+    'DSO22' : 5,
+    'DSO23' : 6,
+    'DSO24' : 7,
+    'time' : 8      #[ps]timestamp of sample
     }
 
-#take only data columns pertaining to Bertha
-data1 = np.genfromtxt(getfile(), skip_header=2, delimiter=',',
-                     usecols=(columns['trigger'], columns['rog1'], 
-                              columns['rog2'], columns['diode'], 
-                              columns['time']))
+#take only 9 data columns (of 10 total) from selected file 
+data = np.genfromtxt(getfile(), skip_header=2, delimiter=',',
+                     usecols=range(1,10))
+# take data pertaining to Bertha
+bertha_data = data[:, (columns['trigger'], columns['rog1'], columns['rog2'], 
+                       columns['diode'], columns['time'])]
 #remove DSO2 rows, where all Bertha signals are nan, avoiding interpolation
-data1 = np.delete(data1, np.isnan(data1[:,0]), 0)
+bertha_data = np.delete(bertha_data, np.isnan(bertha_data[:,0]), 0)
+#NOTE: the slice is mutable, but delete() creates a copy
